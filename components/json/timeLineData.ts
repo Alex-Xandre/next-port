ac914fd6b24ad52eac12dd49575ef4db1fa39d39
alexander <micua.161472@vigan.sti.edu.ph>
2025-01-29 22:39:43 +0800

    Updated timeline-data

--- components/json/timeLineData.ts
+++ components/json/timeLineData.ts
@@ -1,16 +1,24 @@
 export const timeLineData = [
   {
-    title: 'Discovered Freelancing',
-    date: 'December 2021',
+    title: 'Front-End  Stack Developer',
+    date: 'June 2023 - Present',
     description:
-      'December 2021, I Started to get my first client based in California. Created three landing pages for a NFT Projects and a full-stack ELearning system.',
-    ic: '💻',
+      'Currently working as A front-end developer on a Company in Belgium, working closely with the CEO and 2 senior engineers.',
+    ic: '💼',
   },
   {
-    title: 'Graduation',
-    date: 'July 2022',
-    description: 'Graduated Bachelor of Science in Computer Sience at STI College Vigan, Phlippines. ',
-    ic: '🎓',
+    title: 'Lead Developer and Full Stack Developer',
+    date: 'December 2023 - December 2024',
+    description:
+      'I just got hired on big project based on Las Vegas with a current team of 5. Other than this I currently work as a full-stack on a startup agency based on Canada.',
+    ic: '💼',
+  },
+  {
+    title: 'Part-Time Developer',
+    date: 'July 2023 - December 2023',
+    description:
+      'Started to do part-time and project-based Freelance. During this time I somehow got a lot of clients both local and international.',
+    ic: '💻',
   },
   {
     title: 'Front-End Developer',
@@ -21,17 +29,16 @@ export const timeLineData = [
   },
 
   {
-    title: 'Part-Time Developer',
-    date: 'July 2023 - December 2023',
-    description:
-      'Started to do part-time and project-based Freelance. During this time I somehow got a lot of clients both local and international.',
-    ic: '💻',
+    title: 'Graduation',
+    date: 'July 2022',
+    description: 'Graduated Bachelor of Science in Computer Sience at STI College Vigan, Phlippines. ',
+    ic: '🎓',
   },
   {
-    title: 'Lead Developer and Full Stack Developer',
-    date: 'December 2023 - Present',
+    title: 'Discovered Freelancing',
+    date: 'December 2021',
     description:
-      'I just got hired on big project based on Las Vegas with a current team of 5. Other than this I currently work as a full-stack on a startup agency based on Canada.',
-    ic: '💼',
+      'December 2021, I Started to get my first client based in California. Created three landing pages for a NFT Projects and a full-stack ELearning system.',
+    ic: '💻',
   },
 ];
